74a830038f209151c0a322aec8633602c65818ee
Ahmed Ali <ahmed@domar.cloud>
2024-08-29 10:50:48 +0300

    Update screen_size.dart
    adding widtho

--- lib/config/screen_size.dart
+++ lib/config/screen_size.dart
@@ -3,4 +3,6 @@ import 'package:flutter/material.dart';
 class ScreenSize{
   static double height(BuildContext context)=>MediaQuery.of(context).size.height;
   static double width(BuildContext context)=>MediaQuery.of(context).size.width;
+    static double widtho(BuildContext context)=>MediaQuery.of(context).size.width;
+
 }
